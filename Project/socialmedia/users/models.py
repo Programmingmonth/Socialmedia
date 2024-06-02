ef60470e21b562382ea63103240641e2b62c1fff
Williamfox2012 <141017859+Programmingmonth@users.noreply.github.com>
2024-06-02 15:28:57 +0330

    Initial commit

--- Project/socialmedia/users/models.py
+++ Project/socialmedia/users/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
-
+# Create your models here.
 class CustomUser(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
     profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
