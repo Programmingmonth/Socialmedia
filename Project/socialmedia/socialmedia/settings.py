c562df71bedc88397854d09ff0a31213204c1bbb
Williamfox2012 <141017859+Programmingmonth@users.noreply.github.com>
2024-06-03 23:00:13 +0330

    Initial commit

--- Project/socialmedia/socialmedia/settings.py
+++ Project/socialmedia/socialmedia/settings.py
@@ -58,6 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'posts/templates/create_post.html')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
